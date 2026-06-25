b83ad0012fbc0258652e00c3f68ce2da1d29629b
cgeorgako <gf.amaliada@gmail.com>
2026-06-25 07:09:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'lista-v1';
+const ASSETS = [
+  '/lista/',
+  '/lista/index.html',
+  '/lista/manifest.json',
+  '/lista/icon-192.png',
+  '/lista/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(Boolean)))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
