c6f0049db8d957b9cba365f3700b02f1e3f10c6a
cgeorgako <gf.amaliada@gmail.com>
2026-06-27 20:27:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lista-v23';
+const CACHE = 'lista-v24';
 const ASSETS = [
   '/lista/',
   '/lista/index.html',
@@ -9,25 +9,16 @@ const ASSETS = [
   '/lista/recipes.json',
   '/lista/recipes_en.json'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(Boolean)))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS.filter(Boolean))));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
